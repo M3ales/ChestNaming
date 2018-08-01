93f37145340374908fa2cbce49ed54308662474f
Mira <mattfdavies12@gmail.com>
2018-08-01 12:05:33 +0200

    Add project files.

---
+++ ChestNaming/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChestNaming
+{
+    public class Class1
+    {
+    }
+}
