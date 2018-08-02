3cbde9e045269f36ba22a16b378165757606d24e
Mira <mattfdavies12@gmail.com>
2018-08-02 07:39:27 +0200

    Chest tooltips + saving work

---
+++ ChestNaming/LabelledChest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using StardewValley.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChestNaming
+{
+    public class LabelledChest
+    {
+        public int TileX
+        {
+            get;
+            private set;
+        }
+        public int TileY
+        {
+            get;
+            private set;
+        }
+        public string ChestName
+        {
+            get;
+            private set;
+        }
+        public LabelledChest(int tileX, int tileY, string chestName)
+        {
+            TileX = tileX;
+            TileY = tileY;
+            ChestName = chestName;
+        }
+        public bool EqualsChest(Chest chest)
+        {
+            return chest != null && chest.boundingBox.X == TileX && chest.boundingBox.Y == TileY;
+        }
+    }
+}
---
+++ ChestNaming/UI/HoverText.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using StardewValley;
+
+namespace ChestNaming.UI
+{
+    public class HoverText : Frame
+    {
+        private FrameContent Content;
+        public ShadowText Text;
+        public HoverText(int x, int y, Color color, FrameAnchor anchor = FrameAnchor.MidMid, List<IFrameDrawable> components = null) : base(x, y, color, anchor, FrameAnchor.TopLeft, components, null)
+        {
+            Text = new ShadowText("", Game1.smallFont, Game1.textColor);
+            Content = new FrameContent(x, y, color, FrameAnchor.TopLeft, FrameAnchor.TopLeft, xPadding: 20, yPadding: 20)
+            {
+                components = new List<IFrameDrawable>()
+                {
+                    Text
+                }
+            };
+            this.components = new List<IFrameDrawable>() { Content };
+            this.edgeTolerance = 1980;
+            Content.edgeTolerance = 1980;
+
+        }
+    }
+}
