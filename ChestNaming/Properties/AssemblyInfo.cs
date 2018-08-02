98de315f7aa0e39b96f35034f6f4d2eb0dab7578
Mira <mattfdavies12@gmail.com>
2018-08-02 13:19:29 +0200

    Manifest update

--- ChestNaming/Properties/AssemblyInfo.cs
+++ ChestNaming/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ChestNaming")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("M3ales")]
 [assembly: AssemblyProduct("ChestNaming")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
